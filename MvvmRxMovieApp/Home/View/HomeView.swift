c7c6ce9480a89a6ec02b39a1017279e074f18fa1
John_Vera <johnalexiscristobaljimenez6@gmail.com>
2021-02-03 20:28:49 -0600

    TableConectorsPod

--- MvvmRxMovieApp/Home/View/HomeView.swift
+++ MvvmRxMovieApp/Home/View/HomeView.swift
@@ -12,11 +12,25 @@ class HomeView: UIViewController {
     private var route = HomeRouter()
     private var viewModel = HomeViewModel()
     
+    @IBOutlet weak var tableview: UITableView!
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         viewModel.bind(view: self, router: route)
     }
+}
 
+extension HomeView: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        return cell
+    }
 }
